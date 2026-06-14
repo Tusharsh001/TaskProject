c5844d07ea3c76b9246e5c848bcbd42ff6d857cd
Tushar Sharma <sharmatushar2505@gmail.com>
2026-06-14 15:55:23 +0530

    update CORS for production

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://task-project-psi-eight.vercel.app",
   credentials: true
 }));
 app.use(express.json());
